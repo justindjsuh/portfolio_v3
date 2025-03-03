954fd58d7a86c7d4d3ff80264b882f108b0897c9
Justin Suh <justindjsuh@gmail.com>
2025-03-02 19:15:54 -0500

    about enhancements

--- src/components/about/AboutView.tsx
+++ src/components/about/AboutView.tsx
@@ -11,35 +11,11 @@ const AboutView: React.FunctionComponent = () => {
                 initial={{ opacity: 0, y: 100 }} // Start invisible and 50px lower
                 whileInView={{ opacity: 1, y: 0 }} // Fade in and move up
                 viewport={{ once: true, amount: 0.3 }}
-                transition={{ duration: 0.75, ease: "easeOut" }}
+                transition={{ duration: 0.5, ease: "easeOut" }}
             >
                 <h2>
                     Hey there, I'm <span className="highlight">Justin.</span>
                 </h2>
-                {/* <p>
-                    I'm a{" "}
-                    <span className="word-highlight">
-                        full-stack software engineer
-                    </span>{" "}
-                    based in PA, passionate about creating{" "}
-                    <span className="word-highlight">
-                        user-centric, innovative solutions
-                    </span>{" "}
-                    that make a real impact on users. I have a strong drive to
-                    push my skills to new heights and I aim to craft meaningful
-                    products that{" "}
-                    <span className="word-highlight">
-                        truly connect with people.
-                    </span>{" "}
-                    I love what I do, and even recently, I've had the
-                    opportunity to give back by building websites for local
-                    churches, combining my technical expertise with my desire to{" "}
-                    <span className="word-highlight">serve the community.</span>
-                </p> */}
-                {/* <p className="aboutMe">
-                    I blend design, code, and creativity to craft seamless
-                    digital experiences.
-                </p> */}
                 <p className="aboutMe">
                     I bridge the gap between{" "}
                     <span className="word-highlight">creativity</span> and{" "}
@@ -48,7 +24,7 @@ const AboutView: React.FunctionComponent = () => {
                         unforgettable experiences.
                     </span>
                 </p>
-                <p>Thanks for stopping by, and I hope you enjoy your stay.</p>
+                <p>Enjoy your stay.</p>
             </motion.div>
         </div>
     );
